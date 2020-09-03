26d7f042f033d41e276a3cf9dd2e7f6cfa1ea751
iZeroKim <Kimgichuru6@gmail.com>
2020-09-03 18:51:43 +0300

    Added DataBinding

--- app/src/main/java/com/example/leaderboardproject/LearningHoursFragment.kt
+++ app/src/main/java/com/example/leaderboardproject/LearningHoursFragment.kt
@@ -1,28 +1,42 @@
 package com.example.leaderboardproject
 
+import android.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import com.example.leaderboardproject.databinding.LearningHoursFragmentBinding
 import java.util.EnumSet.of
 import java.util.List.of
 
-class LearningHoursFragment : Fragment() {
+class LearningHoursFragment : androidx.fragment.app.Fragment() {
 
     companion object {
         fun newInstance() = LearningHoursFragment()
     }
 
-    private lateinit var viewModel: LearningHoursViewModel
+
+
+    //Lazily initialize LearningHoursViewModel
+    private val viewModel: LearningHoursViewModel by lazy {
+        ViewModelProvider(this).get(LearningHoursViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.learning_hours_fragment, container, false)
+        //Databinding object
+        val binding = LearningHoursFragmentBinding.inflate(inflater)
+
+        //Allow data binding to Obsere LiveData with the lifecycle of this Fragment
+        binding.setLifecycleOwner(this)
+
+        //Give the binding access to LearningHoursViewModel
+        binding.viewModel = viewModel
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
