0a612235d6036fe55fcafed4f2e9d8f0d926a783
iZeroKim <Kimgichuru6@gmail.com>
2020-09-10 19:28:07 +0300

    Created function that submits details on click of the submit button

--- app/src/main/java/com/example/leaderboardproject/SubmitActivity.kt
+++ app/src/main/java/com/example/leaderboardproject/SubmitActivity.kt
@@ -3,9 +3,12 @@ package com.example.leaderboardproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.Toolbar
+import android.widget.*
+import com.example.leaderboardproject.network.submission.SubmitProjectApi
 import com.google.android.material.appbar.AppBarLayout
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SubmitActivity : AppCompatActivity() {
     lateinit var appBarLayout: AppBarLayout
@@ -14,15 +17,35 @@ class SubmitActivity : AppCompatActivity() {
         setContentView(R.layout.activity_submit)
         appBarLayout = findViewById<AppBarLayout>(R.id.appBarSubmit)
         var backArrow = findViewById<ImageView>(R.id.backArrow)
+        var submitBtn = findViewById<Button>(R.id.submitProjectBtn)
         backArrow.setOnClickListener {
             var intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-
+        submitBtn.setOnClickListener {submitProject()}
     }
+
+    fun submitProject(){
+        SubmitProjectApi.retrofitService.submitproject(
+            findViewById<TextView>(R.id.firstname).text.toString(),
+            findViewById<TextView>(R.id.lastname).text.toString(),
+            findViewById<TextView>(R.id.emailaddress).text.toString(),
+            findViewById<TextView>(R.id.githublink).text.toString()
+        ).enqueue(object: Callback<Void>{
+            override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                Toast.makeText(this@SubmitActivity, "Success: ${response.toString()}", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onFailure(call: Call<Void>, t: Throwable) {
+                Toast.makeText(this@SubmitActivity, "Success: ${t.toString()}", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
     }
 }
+
--- app/src/main/java/com/example/leaderboardproject/network/submission/SubmitApi.kt
+++ app/src/main/java/com/example/leaderboardproject/network/submission/SubmitApi.kt
@@ -1,2 +1,48 @@
 package com.example.leaderboardproject.network.submission
 
+import com.example.leaderboardproject.network.skilliq.LearnerIQDetails
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+
+//Define BASE_Url
+private const val BASE_URL = "https://docs.google.com/forms/d/e/"
+
+//Moshi
+private val moshi = Moshi.Builder()
+    //Use this with annotations
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+//Retrofit
+private  val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface SubmitApi {
+    @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
+    @FormUrlEncoded
+    fun submitproject(
+        @Field("entry.1877115667")firstName: String,
+        @Field("entry.2006916086")lastName: String,
+        @Field("entry.1824927963")emailAddress: String,
+        @Field("entry.284483984")githubLink: String
+    ): Call<Void>
+}
+
+//Object to initialize the Retrofit service.
+object SubmitProjectApi{
+    val retrofitService : SubmitApi by lazy {
+        retrofit.create(SubmitApi::class.java)
+    }
+}
